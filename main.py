651c768b65afe9db5f5d0b913201a2b4d5844475
Joel <62688829+jcoffman4@users.noreply.github.com>
2023-05-02 18:09:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# Determines whether a character is a letter, and if so how much to shift it left/right
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+# Prints the 'Caesar Cipher' wording onto screen from art.py file
+print(art.logo)
+
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    # Adjusts all numbers for shifts over 26 (letters in alphabet)
+    while shift > 26:
+        shift = shift - 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    # Prompts the user asking if they'd like to continue or quit
+    result = input('Would you like to continue? ')
+    if result == 'no':
+        should_continue = False
+        print('Program has ended')
